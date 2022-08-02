15e68550075017168876dd78138530b83f5036f3
suyashsahu3 <suyashsahu3@gmail.com>
2022-08-02 17:06:12 +0530

    token auth

---
+++ src/config/logging.ts
@@ -0,0 +1,26 @@
+const info = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
+    } else {
+        console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`);
+    }
+};
+
+
+const error = (namespace: string, message: string, object?: any) => {
+    if (object) {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`, object);
+    } else {
+        console.error(`[${getTimeStamp()}] [ERROR] [${namespace}] ${message}`);
+    }
+};
+
+
+const getTimeStamp = (): string => {
+    return new Date().toISOString();
+};
+
+export default {
+    info,
+    error
+};
